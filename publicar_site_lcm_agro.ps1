ca3eb22989fe1942762a492d693afb5e74a18ad6
FelipeKatriel <felipekdelera@gmail.com>
2025-05-07 19:28:56 -0300

    Site HTML puro com estrutura limpa

---
+++ publicar_site_lcm_agro.ps1
@@ -0,0 +1,9 @@
+
+cd "C:\Users\julio\Desktop\lcm_agro_site"
+
+git init
+git remote add origin https://github.com/FelipeKatriel/site-lcm-agro.git
+git branch -M main
+git add .
+git commit -m "Site HTML puro com estrutura limpa"
+git push -u origin main
